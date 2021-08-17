43fa6330597744e95dd2e7dbdfb3beec7bbbf775
meme2367 <meme91322367@gmail.com>
2021-08-17 13:21:26 +0900

    feat : Add initial code

---
+++ src/main/java/com/mindDiary/mindDiary/MindDiaryApplication.java
@@ -0,0 +1,13 @@
+package com.mindDiary.mindDiary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MindDiaryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MindDiaryApplication.class, args);
+	}
+
+}
