378a36af6f2f5c4dafc21cb99e8f8d4a0044ad89
meme2367 <meme91322367@gmail.com>
2021-08-18 15:07:43 +0900

    refactor : refactor 중복 유저 확인 테스트

--- src/test/java/com/mindDiary/mindDiary/service/UserServiceTest.java
+++ src/test/java/com/mindDiary/mindDiary/service/UserServiceTest.java
@@ -1,17 +1,24 @@
 package com.mindDiary.mindDiary.service;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.doReturn;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.mindDiary.mindDiary.controller.UserController;
 import com.mindDiary.mindDiary.domain.User;
 import com.mindDiary.mindDiary.utils.RedisUtil;
 import java.util.UUID;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -19,6 +26,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
 @AutoConfigureMockMvc
 @SpringBootTest
@@ -42,24 +51,39 @@ public class UserServiceTest {
   @Value("${mailInfo.email}")
   private String email;
 
-  @Test
-  @DisplayName("회원가입 실패 : 중복 닉네임, 중복 이메일")
-  public void joinFail() throws Exception {
-    User user = new User();
-    user.setPassword("ssss");
-    user.setEmail("meme@naver.com");
-    user.setNickname("aaaaa");
+  @InjectMocks
+  private UserController userController;
 
-    String url = "/auth/join";
-    String content = objectMapper.writeValueAsString(user);
-    mockMvc
-        .perform(post(url)
-            .content(content)
-            .contentType(MediaType.APPLICATION_JSON))
-        .andDo(print())
-        .andExpect(status().isConflict());
+  @Mock
+  private UserService userService;
+
+  @BeforeEach
+  public void init() {
+    mockMvc = MockMvcBuilders.standaloneSetup(userController).build();
   }
 
+
+  @Test
+  @DisplayName("회원가입 실패 : 중복 닉네임")
+  public void joinFail() throws Exception {
+    //given
+    User user = new User();
+    user.setPassword("new");
+    user.setEmail("new@naver.com");
+    user.setNickname("구우");
+    doReturn(true).when(userService).isDuplicate(any(User.class));
+
+    //when
+    String content = objectMapper.writeValueAsString(user);
+    String url = "/auth/join";
+    ResultActions resultActions = mockMvc
+        .perform(post(url)
+            .content(content)
+            .contentType(MediaType.APPLICATION_JSON));
+
+    //then
+    resultActions.andDo(print()).andExpect(status().isConflict());
+  }
   @Test
   @DisplayName("bcrypt 비밀번호 생성 및 매칭 테스트")
   public void passwordEncoder() {
