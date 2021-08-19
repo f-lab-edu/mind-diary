3e7621684bd9e1a6a818d2784e199137304764d3
meme2367 <meme91322367@gmail.com>
2021-08-19 17:48:44 +0900

    test : User 컨트롤러 테스트 수정

--- src/test/java/com/mindDiary/mindDiary/controller/UserControllerTest.java
+++ src/test/java/com/mindDiary/mindDiary/controller/UserControllerTest.java
@@ -6,23 +6,30 @@ import static org.mockito.Mockito.doReturn;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.web.servlet.function.RequestPredicates.contentType;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.mindDiary.mindDiary.controller.UserController;
 import com.mindDiary.mindDiary.domain.User;
 import com.mindDiary.mindDiary.dto.request.UserJoinRequestDTO;
+import com.mindDiary.mindDiary.dto.request.UserLoginRequestDTO;
 import com.mindDiary.mindDiary.service.UserService;
+import com.mindDiary.mindDiary.strategy.JwtStrategyTest;
+import com.mindDiary.mindDiary.strategy.jwt.JwtStrategy;
 import java.util.UUID;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Spy;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
@@ -43,38 +50,61 @@ public class UserControllerTest {
   @Mock
   private UserService userService;
 
-  @Value("${mailInfo.email}")
-  private String email;
+  @Mock
+  private PasswordEncoder passwordEncoder;
 
-  private UserJoinRequestDTO userJoinRequestDTO;
+  @Mock
+  private JwtStrategy jwtStrategy;
 
-  private User user;
+  private static final String EMAIL = "email@google.com";
+
+  private static final String PASSWORD = "password";
+
+  private static final String NICKNAME = "nickname";
+
+  private static final String JOIN_URL =  "/auth/join";
+
+  private static final String LOGIN_URL = "/auth/login";
+
+  private static final String CHECK_EMAIL_TOKEN_URL = "/auth/check-email-token";
 
   @BeforeEach
   public void init() {
     mockMvc = MockMvcBuilders.standaloneSetup(userController).build();
-    userJoinRequestDTO = new UserJoinRequestDTO();
-    userJoinRequestDTO.setPassword("new");
-    userJoinRequestDTO.setEmail("new@naver.com");
-    userJoinRequestDTO.setNickname("nickname");
-
-    user = new User();
-    user.setPassword("new");
-    user.setEmail("new@naver.com");
-    user.setNickname("nickname");
   }
+
+  public UserJoinRequestDTO getUserJoinRequestDTO() {
+    UserJoinRequestDTO userJoinRequestDTO = new UserJoinRequestDTO();
+    userJoinRequestDTO.setPassword(PASSWORD);
+    userJoinRequestDTO.setEmail(EMAIL);
+    userJoinRequestDTO.setNickname(NICKNAME);
+    return userJoinRequestDTO;
+  }
+
+  public User getUser() {
+    User user = new User();
+    user.setPassword(PASSWORD);
+    user.setEmail(EMAIL);
+    user.setNickname(NICKNAME);
+    return user;
+  }
+
+  public UserLoginRequestDTO getUserLoginRequestDTO() {
+    UserLoginRequestDTO userLoginRequestDTO = new UserLoginRequestDTO();
+    userLoginRequestDTO.setPassword(PASSWORD);
+    userLoginRequestDTO.setEmail(EMAIL);
+    return userLoginRequestDTO;
+  }
+
 
   @Test
   @DisplayName("회원가입 실패 : 중복 이메일 유저")
   public void joinFailByEmail() throws Exception {
-    doReturn(user).when(userService).findByEmail(userJoinRequestDTO.getEmail());
-    doReturn(null).when(userService).findByNickname(userJoinRequestDTO.getNickname());
-    doNothing().when(userService).join(userJoinRequestDTO);
+    doReturn(getUser()).when(userService).findByEmail(EMAIL);
 
-    String content = objectMapper.writeValueAsString(user);
-    String url = "/auth/join";
+    String content = objectMapper.writeValueAsString(getUserJoinRequestDTO());
     ResultActions resultActions = mockMvc
-        .perform(post(url)
+        .perform(post(JOIN_URL)
             .content(content)
             .contentType(MediaType.APPLICATION_JSON));
 
@@ -84,15 +114,14 @@ public class UserControllerTest {
   @Test
   @DisplayName("회원가입 실패 : 중복 닉네임 유저")
   public void joinFailByNickname() throws Exception {
+    User user = getUser();
 
-    doReturn(null).when(userService).findByEmail(userJoinRequestDTO.getEmail());
-    doReturn(user).when(userService).findByNickname(userJoinRequestDTO.getNickname());
-    doNothing().when(userService).join(userJoinRequestDTO);
+    doReturn(null).when(userService).findByEmail(EMAIL);
+    doReturn(user).when(userService).findByNickname(NICKNAME);
 
     String content = objectMapper.writeValueAsString(user);
-    String url = "/auth/join";
     ResultActions resultActions = mockMvc
-        .perform(post(url)
+        .perform(post(JOIN_URL)
             .content(content)
             .contentType(MediaType.APPLICATION_JSON));
 
@@ -102,14 +131,15 @@ public class UserControllerTest {
   @Test
   @DisplayName("회원가입 성공")
   public void join() throws Exception {
-    doReturn(null).when(userService).findByEmail(userJoinRequestDTO.getEmail());
-    doReturn(null).when(userService).findByNickname(userJoinRequestDTO.getNickname());
+    UserJoinRequestDTO userJoinRequestDTO = getUserJoinRequestDTO();
+
+    doReturn(null).when(userService).findByEmail(EMAIL);
+    doReturn(null).when(userService).findByNickname(NICKNAME);
     doNothing().when(userService).join(userJoinRequestDTO);
 
     String content = objectMapper.writeValueAsString(userJoinRequestDTO);
-    String url = "/auth/join";
     ResultActions resultActions = mockMvc
-        .perform(post(url)
+        .perform(post(JOIN_URL)
             .content(content)
             .contentType(MediaType.APPLICATION_JSON));
 
@@ -123,9 +153,8 @@ public class UserControllerTest {
     String email = "xxxx";
     doReturn(true).when(userService).checkEmailToken(token, email);
 
-    String url = "/auth/check-email-token";
     ResultActions resultActions = mockMvc
-        .perform(get(url)
+        .perform(get(CHECK_EMAIL_TOKEN_URL)
             .param("token",token)
             .param("email",email));
 
@@ -135,13 +164,13 @@ public class UserControllerTest {
   @Test
   @DisplayName("인증 메일 전송 실패 : 입력값 오류")
   public void sendMailFail() throws Exception {
-    String url = "/auth/check-email-token";
+
     String token = "xxxx";
     String email = "xxxx";
     doReturn(false).when(userService).checkEmailToken(token, email);
 
     ResultActions resultActions = mockMvc
-        .perform(get(url)
+        .perform(get(CHECK_EMAIL_TOKEN_URL)
             .param("token",token)
             .param("email",email));
 
@@ -149,4 +178,45 @@ public class UserControllerTest {
 
   }
 
+  @Test
+  @DisplayName("로그인 성공")
+  public void loginSuccess() throws Exception {
+    //작성중
+
+  }
+
+  @Test
+  @DisplayName("로그인 실패 : 일치하는 유저가 DB에 없을 경우 확인")
+  public void loginFailByUserNotExist() throws Exception {
+
+    UserLoginRequestDTO userLoginRequestDTO = getUserLoginRequestDTO();
+    doReturn(null).when(userService).findByEmail(EMAIL);
+
+    String content = objectMapper.writeValueAsString(userLoginRequestDTO);
+    ResultActions resultActions = mockMvc
+        .perform(post(LOGIN_URL)
+            .content(content)
+            .contentType(MediaType.APPLICATION_JSON));
+
+    resultActions.andDo(print()).andExpect(status().isBadRequest());
+  }
+
+  @Test
+  @DisplayName("로그인 실패 : 비밀번호가 틀릴 경우 확인")
+  public void loginFailByWrongPassword() throws Exception {
+
+    UserLoginRequestDTO userLoginRequestDTO = getUserLoginRequestDTO();
+    User user = getUser();
+
+    doReturn(user).when(userService).findByEmail(EMAIL);
+    doReturn(false).when(passwordEncoder).matches(user.getPassword(), userLoginRequestDTO.getPassword());
+
+    String content = objectMapper.writeValueAsString(userLoginRequestDTO);
+    ResultActions resultActions = mockMvc
+        .perform(post(LOGIN_URL)
+            .content(content)
+            .contentType(MediaType.APPLICATION_JSON));
+
+    resultActions.andDo(print()).andExpect(status().isBadRequest());
+  }
 }
