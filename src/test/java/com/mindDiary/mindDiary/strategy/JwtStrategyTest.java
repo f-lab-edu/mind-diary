ffea8bf392b18aeed9ebd66a1506cca77528247b
meme2367 <meme91322367@gmail.com>
2021-08-19 12:51:55 +0900

    test : JWT 토큰 유효성 검증 테스트 코드 작성

--- src/test/java/com/mindDiary/mindDiary/strategy/JwtStrategyTest.java
+++ src/test/java/com/mindDiary/mindDiary/strategy/JwtStrategyTest.java
@@ -17,15 +17,30 @@ public class JwtStrategyTest {
   @Test
   @DisplayName("token으로 userId 가져오는지 확인")
   public void getUserIdByToken() {
-    String token = jwtUtil.createToken(35,1,600);
+    String token = jwtUtil.createToken(35,1,"meme@naver.com",600);
     assertThat(jwtUtil.getUserId(token)).isSameAs(35);
   }
 
   @Test
   @DisplayName("token으로 userRole 가져오는지 확인")
   public void getUserRoleByToken() {
-    String token = jwtUtil.createToken(1,5,600);
+    String token = jwtUtil.createToken(1,1,"meme@naver.com",600);
     assertThat(jwtUtil.getUserRole(token)).isSameAs(5);
+  }
+
+  @Test
+  @DisplayName("token으로 이메일 가져오는지 확인")
+  public void getUserEmailByToken() {
+    String email = "meme@naver.com";
+    String token = jwtUtil.createToken(1,1,email,600);
+    assertThat(jwtUtil.getUserEmail(token)).isEqualTo(email);
+  }
+
+  @Test
+  @DisplayName("토큰 유효성 확인")
+  public void validateToken() {
+    String token = jwtUtil.createToken(1, 1,"meme@naver.com", 0);
+    assertThat(jwtUtil.validateToken(token)).isFalse();
   }
 
 }
