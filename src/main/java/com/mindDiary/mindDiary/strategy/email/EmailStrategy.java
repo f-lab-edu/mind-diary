9566d37c45fb510c18eff74fc2252ed9df4a2f1d
meme2367 <meme91322367@gmail.com>
2021-08-31 21:03:12 +0900

    refactor : 이메일 전송과 레디스 값 추가 validate 메서드 작성

--- src/main/java/com/mindDiary/mindDiary/strategy/email/EmailStrategy.java
+++ src/main/java/com/mindDiary/mindDiary/strategy/email/EmailStrategy.java
@@ -1,5 +1,9 @@
 package com.mindDiary.mindDiary.strategy.email;
 
+import com.mindDiary.mindDiary.dto.UserDTO;
+
 public interface EmailStrategy {
   void sendMessage(String toEmailAddress, String emailCheckToken);
+
+  void sendUserJoinMessage(UserDTO user);
 }
--- src/main/java/com/mindDiary/mindDiary/strategy/redis/RedisStrategy.java
+++ src/main/java/com/mindDiary/mindDiary/strategy/redis/RedisStrategy.java
@@ -1,6 +1,9 @@
 package com.mindDiary.mindDiary.strategy.redis;
 
 
+import com.mindDiary.mindDiary.dto.TokenDTO;
+import com.mindDiary.mindDiary.dto.UserDTO;
+
 public interface RedisStrategy {
 
   void setValue(String key, String value);
@@ -10,4 +13,12 @@ public interface RedisStrategy {
   void setValue(String key, String value, long duration);
 
   void deleteValue(String key);
+
+  void validateValue(String key, String value);
+
+  void validateDuration(long duration);
+
+  void addRefreshToken(TokenDTO token, UserDTO findUser);
+
+  void addEmailToken(UserDTO user);
 }
