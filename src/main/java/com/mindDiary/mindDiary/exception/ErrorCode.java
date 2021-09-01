ee2c464f778d54b25ed67d40da670b0275766b95
meme2367 <meme91322367@gmail.com>
2021-09-01 15:07:59 +0900

    feat : 사용자 권한 거부 예외 코드 생성

--- src/main/java/com/mindDiary/mindDiary/exception/ErrorCode.java
+++ src/main/java/com/mindDiary/mindDiary/exception/ErrorCode.java
@@ -10,7 +10,8 @@ public enum ErrorCode {
   NOT_MATCHED_PASSWORD(400, "비밀번호가 일치하지 않습니다"),
   NOT_MATCHED_ID(403, "ID가 일치하지 않습니다"),
   REDIS_ADD_VALUE_EXCEPTION(500, "캐시 저장 예외가 발생했습니다"),
-  MAIL_SEND_FAILED_EXCEPTION(500, "이메일 전송에 실패했습니다");
+  MAIL_SEND_FAILED_EXCEPTION(500, "이메일 전송에 실패했습니다"),
+  PERMISSION_DENIED_EXCEPTION(403, "권한이 거부되었습니다");
 
   private int status;
   private String message;
