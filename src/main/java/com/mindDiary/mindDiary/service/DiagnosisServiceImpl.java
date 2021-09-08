f0af01ff12c09cabd3ecb6c3218f49e75c4ad87d
meme2367 <meme91322367@gmail.com>
2021-09-08 21:54:16 +0900

    refactor : Diagnosis 서비스 코드 수정

--- src/main/java/com/mindDiary/mindDiary/service/DiagnosisServiceImpl.java
+++ src/main/java/com/mindDiary/mindDiary/service/DiagnosisServiceImpl.java
@@ -1,22 +1,16 @@
 package com.mindDiary.mindDiary.service;
 
-import com.mindDiary.mindDiary.dto.request.CreateDiagnosisResultRequestDTO;
-import com.mindDiary.mindDiary.dto.request.QuestionAnswerRequestDTO;
-import com.mindDiary.mindDiary.dto.response.ReadDiagnosisResultResponseDTO;
-import com.mindDiary.mindDiary.dto.response.ReadDiagnosisResponseDTO;
 import com.mindDiary.mindDiary.entity.Diagnosis;
 import com.mindDiary.mindDiary.entity.DiagnosisScore;
 import com.mindDiary.mindDiary.entity.DiagnosisWithQuestion;
 import com.mindDiary.mindDiary.entity.Question;
-import com.mindDiary.mindDiary.entity.Question.Reverse;
-import com.mindDiary.mindDiary.entity.QuestionAnswer;
+import com.mindDiary.mindDiary.entity.Answer;
 import com.mindDiary.mindDiary.entity.QuestionBaseLine;
 import com.mindDiary.mindDiary.entity.UserDiagnosis;
 import com.mindDiary.mindDiary.repository.DiagnosisRepository;
+import com.mindDiary.mindDiary.strategy.ScoreCalculateStrategy;
 import java.time.LocalDateTime;
-import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.IntStream;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -27,77 +21,42 @@ import org.springframework.stereotype.Service;
 public class DiagnosisServiceImpl implements DiagnosisService {
 
   private final DiagnosisRepository diagnosisRepository;
+  private final QuestionBaseLineService questionBaseLineService;
+  private final QuestionService questionService;
+  private final DiagnosisScoreService diagnosisScoreService;
+  private final UserDiagnosisService userDiagnosisService;
 
   @Override
-  public List<ReadDiagnosisResponseDTO> readDiagnosis() {
-    List<Diagnosis> diagnosisList = diagnosisRepository.findDiagnosis();
-
-    List<ReadDiagnosisResponseDTO> readDiagnosisResponses = new ArrayList<>();
-
-    diagnosisList.forEach(diagnosis -> readDiagnosisResponses.add(
-        ReadDiagnosisResponseDTO.create(diagnosis)));
-
-    return readDiagnosisResponses;
+  public List<Diagnosis> readDiagnosis() {
+    return diagnosisRepository.findDiagnosis();
   }
 
   @Override
-  public ReadDiagnosisResponseDTO readDiagnosisQuestions(int diagnosisId) {
-
-    DiagnosisWithQuestion diagnosisWithQuestion
-        = diagnosisRepository.findDiagnosisWithQuestionById(diagnosisId);
-
-    return ReadDiagnosisResponseDTO.create(diagnosisWithQuestion);
+  public DiagnosisWithQuestion readDiagnosisQuestions(int diagnosisId) {
+    return diagnosisRepository.findDiagnosisWithQuestionById(diagnosisId);
   }
 
+
   @Override
-  public ReadDiagnosisResultResponseDTO createDiagnosisResult(int diagnosisId,
-      CreateDiagnosisResultRequestDTO createDiagnosisResultRequest, int userId) {
+  public UserDiagnosis createDiagnosisResult(int diagnosisId,
+      List<Answer> answers, int userId) {
 
-    List<QuestionAnswer> questionAnswer = new ArrayList<>();
-    for (QuestionAnswerRequestDTO qa : createDiagnosisResultRequest.getQuestionAnswerRequests()) {
-      questionAnswer.add(QuestionAnswer.create(qa.getQuestionId(), qa.getChoiceNumber()));
-    }
+    List<QuestionBaseLine> questionBaseLines = questionBaseLineService.readByDiagnosisId(diagnosisId);
+    List<Question> questions = questionService.readByAnswers(answers);
 
-    List<QuestionBaseLine> questionBaseLines
-        = diagnosisRepository.findQuestionBaseLinesById(diagnosisId);
+    ScoreCalculateStrategy calculateStrategy = ScoreCalculateStrategy.createScores(questionBaseLines);
 
-    List<Question> questions = diagnosisRepository.findQuestionsByAnswers(questionAnswer);
+    int score = questions.stream()
+        .mapToInt(q -> calculateStrategy.calc(q.getChoiceNumber(), q.getReverse()))
+        .sum();
 
-    int score = 0;
-    int questionSize = questions.size();
-    int baseLineSize = questionBaseLines.size();
-
-    for (int i = 0; i < questionSize; i++) {
-      Question q = questions.get(i);
-      int choiceNumber = questionAnswer.get(i).getChoiceNumber();
-      if (q.getReverse() == Reverse.TRUE) {
-        score += questionBaseLines.get(baseLineSize - choiceNumber - 1).getScore();
-      } else {
-        score += questionBaseLines.get(choiceNumber).getScore();
-      }
-    }
-
-    DiagnosisScore diagnosisScores = diagnosisRepository.findDiagnosisScore(diagnosisId, score);
-    String content = diagnosisScores.getContent();
+    DiagnosisScore diagnosisScore = diagnosisScoreService.readOneByDiagnosisIdAndScore(diagnosisId, score);
+    String content = diagnosisScore.getContent();
 
     UserDiagnosis userDiagnosis = UserDiagnosis
         .create(userId, diagnosisId, LocalDateTime.now(), score, content);
-    diagnosisRepository.save(userDiagnosis);
+    userDiagnosisService.save(userDiagnosis);
 
-    ReadDiagnosisResultResponseDTO diagnosisResultScoreResponse
-        = ReadDiagnosisResultResponseDTO.create(userDiagnosis);
-
-    return diagnosisResultScoreResponse;
-  }
-
-  @Override
-  public List<ReadDiagnosisResultResponseDTO> readMyDiagnosisResults(int userId) {
-    List<UserDiagnosis> userDiagnosis = diagnosisRepository.findUserDiagnosisById(userId);
-
-    List<ReadDiagnosisResultResponseDTO> myDiagnosisResultReponses = new ArrayList<>();
-    userDiagnosis.forEach(ud -> myDiagnosisResultReponses.add(
-        ReadDiagnosisResultResponseDTO.create(ud)));
-
-    return myDiagnosisResultReponses;
+    return userDiagnosis;
   }
 }
