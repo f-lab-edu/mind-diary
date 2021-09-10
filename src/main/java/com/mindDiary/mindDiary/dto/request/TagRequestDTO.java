17a30afc5e0a9c579069b811637170929dd5480a
meme2367 <meme91322367@gmail.com>
2021-09-10 13:42:48 +0900

    feat : 게시글 작성 request DTO 코드 작성

---
+++ src/main/java/com/mindDiary/mindDiary/dto/request/TagRequestDTO.java
@@ -0,0 +1,13 @@
+package com.mindDiary.mindDiary.dto.request;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@ToString
+@Getter
+@Setter
+public class TagRequestDTO {
+
+  private String name;
+}
