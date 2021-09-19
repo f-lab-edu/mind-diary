aab08a45406c19edae4c5af78088570a27045c02
meme2367 <meme91322367@gmail.com>
2021-09-19 21:09:51 +0900

    refactor : Repository save 코드 수정

--- src/main/java/com/mindDiary/mindDiary/repository/TagRepository.java
+++ src/main/java/com/mindDiary/mindDiary/repository/TagRepository.java
@@ -1,12 +1,13 @@
 package com.mindDiary.mindDiary.repository;
 
 import com.mindDiary.mindDiary.entity.Tag;
+import java.util.List;
 import org.apache.ibatis.annotations.Mapper;
 
 @Mapper
 public interface TagRepository {
 
-  int save(Tag tag);
-
   Tag findByName(String name);
+
+  int save(List<Tag> tags);
 }
