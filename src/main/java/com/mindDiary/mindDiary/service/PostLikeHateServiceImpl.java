98bb8ce82800da1a200d536624902917c9bbc36f
meme2367 <meme91322367@gmail.com>
2021-09-29 14:21:20 +0900

    refactor : 게시글 좋아요 싫어요 관련 서비스  코드 수정

--- src/main/java/com/mindDiary/mindDiary/service/PostLikeHateServiceImpl.java
+++ src/main/java/com/mindDiary/mindDiary/service/PostLikeHateServiceImpl.java
@@ -1,9 +1,11 @@
 package com.mindDiary.mindDiary.service;
 
 import com.mindDiary.mindDiary.repository.PostLikeHateRepository;
+import com.mindDiary.mindDiary.repository.PostRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Slf4j
 @Service
@@ -11,14 +13,19 @@ import org.springframework.stereotype.Service;
 public class PostLikeHateServiceImpl implements PostLikeHateService {
 
   private final PostLikeHateRepository postLikeHateRepository;
+  private final PostRepository postRepository;
 
   @Override
+  @Transactional
   public void createLike(int postId, int userId) {
     postLikeHateRepository.saveLike(postId, userId);
+    postRepository.increaseLikeCount(postId);
   }
 
   @Override
+  @Transactional
   public void createHate(int postId, int userId) {
     postLikeHateRepository.saveHate(postId, userId);
+    postRepository.increaseHateCount(postId);
   }
 }
