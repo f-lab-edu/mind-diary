f56111bfd736862f6c36b3b097e045fbec475d44
meme2367 <meme91322367@gmail.com>
2021-10-05 12:49:14 +0900

    refactor : Redis 설정 수정

--- src/main/java/com/mindDiary/mindDiary/config/RedisConfiguration.java
+++ src/main/java/com/mindDiary/mindDiary/config/RedisConfiguration.java
@@ -1,18 +1,23 @@
 package com.mindDiary.mindDiary.config;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jdk8.Jdk8Module;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import java.time.Duration;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.cache.RedisCacheManager;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
-import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
-import org.springframework.data.redis.serializer.RedisSerializer;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializationContext;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
-@EnableRedisRepositories
 public class RedisConfiguration {
 
   @Value("${spring.redis.host}")
@@ -27,13 +32,46 @@ public class RedisConfiguration {
   }
 
   @Bean
+  public ObjectMapper objectMapper() {
+    ObjectMapper mapper = new ObjectMapper();
+    mapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+    mapper.registerModules(new JavaTimeModule(), new Jdk8Module());
+    return mapper;
+  }
+
+  @Bean
   public RedisTemplate<String, Object> redisTemplate() {
+
+    GenericJackson2JsonRedisSerializer serializer =
+        new GenericJackson2JsonRedisSerializer(objectMapper());
+
     RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
-    redisTemplate.setKeySerializer(RedisSerializer.string());
-    redisTemplate.setValueSerializer(RedisSerializer.string());
-    redisTemplate.setHashKeySerializer(RedisSerializer.string());
-    redisTemplate.setHashValueSerializer(RedisSerializer.string());
     redisTemplate.setConnectionFactory(redisConnectionFactory());
+    redisTemplate.setKeySerializer(new StringRedisSerializer());
+    redisTemplate.setValueSerializer(serializer);
+    redisTemplate.setHashKeySerializer(new StringRedisSerializer());
+    redisTemplate.setHashValueSerializer(serializer);
     return redisTemplate;
   }
+
+  @Bean
+  public RedisCacheManager redisCacheManager() {
+
+    RedisCacheConfiguration redisCacheConfiguration = RedisCacheConfiguration
+        .defaultCacheConfig()
+        .serializeKeysWith(
+            RedisSerializationContext.SerializationPair
+                .fromSerializer(new StringRedisSerializer()))
+        .serializeValuesWith(
+            RedisSerializationContext.SerializationPair
+                .fromSerializer(new GenericJackson2JsonRedisSerializer(objectMapper()))
+        )
+        .entryTtl(Duration.ofDays(1L));
+
+    return RedisCacheManager.RedisCacheManagerBuilder
+        .fromConnectionFactory(redisConnectionFactory())
+        .cacheDefaults(redisCacheConfiguration)
+        .build();
+  }
+
 }
