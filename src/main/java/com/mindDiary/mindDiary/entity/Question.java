1821d68ab893c92de8a6094a6f109b512f6c0450
meme2367 <meme91322367@gmail.com>
2021-10-24 11:56:25 +0900

    refactor : 자가진단 생성에 따른 Entity 코드 수정

--- src/main/java/com/mindDiary/mindDiary/entity/Question.java
+++ src/main/java/com/mindDiary/mindDiary/entity/Question.java
@@ -1,10 +1,11 @@
 package com.mindDiary.mindDiary.entity;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
-
+@Builder
 @ToString
 @Getter
 @AllArgsConstructor
