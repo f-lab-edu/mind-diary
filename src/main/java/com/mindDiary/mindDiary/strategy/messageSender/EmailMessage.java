366d2675162a9d9a87d3c3677282377368c46132
meme2367 <meme91322367@gmail.com>
2022-01-02 07:02:30 +0900

    refactor : EmailMessage 인터페이스 및 구현 클래스 수정

--- src/main/java/com/mindDiary/mindDiary/strategy/messageSender/EmailMessage.java
+++ src/main/java/com/mindDiary/mindDiary/strategy/messageSender/EmailMessage.java
@@ -1,40 +1,42 @@
 package com.mindDiary.mindDiary.strategy.messageSender;
 
 import com.mindDiary.mindDiary.exception.businessException.MailSendFailedException;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.ibatis.javassist.Loader.Simple;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
 @Slf4j
-@Service
+@RequiredArgsConstructor
+@Component
 public class EmailMessage implements MessageSender {
 
   @Value("${mailInfo.email}")
-  private String fromEmailAddress;
-
+  private final String fromEmailAddress;
   private static final String URL = "http://localhost:8080/auth/check-email-token";
   private static final String SUBJECT = "Mind-diary 회원 가입 인증";
   private static final int ZERO = 0;
-
-  @Autowired
-  private JavaMailSender javaMailSender;
+  private final JavaMailSender javaMailSender;
 
   @Override
   public void sendMessage(String toEmailAddress, String text) {
-
     validateString(toEmailAddress);
     validateString(text);
-
+    SimpleMailMessage message = createMessage(toEmailAddress, text);
+    javaMailSender.send(message);
+  }
+  private SimpleMailMessage createMessage(String toEmailAddress, String text) {
     SimpleMailMessage message = new SimpleMailMessage();
     message.setFrom(fromEmailAddress);
     message.setTo(toEmailAddress);
     message.setSubject(SUBJECT);
     message.setText(URL + text);
-    javaMailSender.send(message);
-
+    return message;
   }
 
 
@@ -42,13 +44,6 @@ public class EmailMessage implements MessageSender {
     if (str == null || str.length() == ZERO) {
       throw new MailSendFailedException();
     }
-  }
-
-  @Override
-  public void sendUserJoinMessage(String emailToken, String email) {
-    String text = "?token=" + emailToken +
-        "&email=" + email;
-    sendMessage(email, text);
   }
 
 }
