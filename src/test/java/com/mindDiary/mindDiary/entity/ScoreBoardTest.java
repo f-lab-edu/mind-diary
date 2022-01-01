30cc43bb44c2751feb65bdcacbc36f2710c6c08d
meme2367 <meme91322367@gmail.com>
2022-01-01 21:56:17 +0900

    feat : 점수판 클래스 테스트코드 작성

---
+++ src/test/java/com/mindDiary/mindDiary/entity/ScoreBoardTest.java
@@ -0,0 +1,30 @@
+package com.mindDiary.mindDiary.entity;
+
+import static com.mindDiary.mindDiary.service.diagnosis.DiagnosisDummy.makeAnswerList;
+import static com.mindDiary.mindDiary.service.diagnosis.DiagnosisDummy.makeBaseLineList;
+import static com.mindDiary.mindDiary.service.diagnosis.DiagnosisDummy.makeQuestionList;
+import static org.assertj.core.api.Assertions.assertThat;
+
+import com.mindDiary.mindDiary.strategy.Calculator2.Calculator;
+import com.mindDiary.mindDiary.strategy.Calculator2.ScoreCalculator2;
+import java.util.List;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class ScoreBoardTest {
+
+  @Test
+  @DisplayName("점수 계산 시 18점이 나와야한다.")
+  void calculate() {
+    List<Question> questions = makeQuestionList();
+    List<QuestionBaseLine> baseLines  = makeBaseLineList();
+
+    List<Answer> answers = makeAnswerList();
+    ScoreCalculator2 calculator2 = new Calculator();
+    ScoreBoard scoreBoard1 = new ScoreBoard(questions, baseLines);
+
+    assertThat(scoreBoard1.calc(answers, calculator2)).isSameAs(18);
+  }
+
+
+}
